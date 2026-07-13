d4d8ba48219b4ab7c5dfa87aa5cff6e547f08208
rahmafathi <rahmafathi006@gmail.com>
2026-07-13 20:55:43 +0300

    first commit

---
+++ Bootstrap/startbootstrap-sb-admin-2-gh-pages/include/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$server="localhost";
+$username="root";
+$pass="";
+$databaseName="sb_admin2";
+
+$connect= new mysqli($server , $username , $pass , $databaseName);
+
+if($connect->connect_error){
+    die("connection_error".$connect->connect_error);
+}else{
+echo "conected...";     
+}
+?>
